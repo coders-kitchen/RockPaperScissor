df03fb45e48966b5eef2d5ed2ea32aaf9f88ff05
Peter Daum <peter@coders-kitchen.com>
2015-03-17 21:35:39 +0100

    Changed random implementation

--- src/main/java/com/coderskitchen/rockpaperscissor/game/player/RandomGestureChoosingStrategy.java
+++ src/main/java/com/coderskitchen/rockpaperscissor/game/player/RandomGestureChoosingStrategy.java
@@ -1,20 +1,22 @@
 package com.coderskitchen.rockpaperscissor.game.player;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Peter on 15.03.2015.
  */
 public class RandomGestureChoosingStrategy implements GestureChoosingStrategy {
 
-	public static final int FIRST_GESTURE = 0;
+    private Random random;
 
-	@Override
+    public RandomGestureChoosingStrategy() {
+        random = new Random();
+    }
+
+    @Override
     public Gesture chooseGesture() {
-        List<Gesture> gestures = Arrays.asList(Gesture.values());
-        Collections.shuffle(gestures);
-        return gestures.get(FIRST_GESTURE);
+        Gesture[] gestures = Gesture.values();
+        int randomGestureIndex = random.nextInt(gestures.length);
+        return gestures[randomGestureIndex];
     }
 }
