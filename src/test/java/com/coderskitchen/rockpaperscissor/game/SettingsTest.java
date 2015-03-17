23fc910701241a0df9683b5e01065934c26642e7
Peter Daum <peter@coders-kitchen.com>
2015-03-17 21:53:26 +0100

    Fixed tests

--- src/test/java/com/coderskitchen/rockpaperscissor/game/SettingsTest.java
+++ src/test/java/com/coderskitchen/rockpaperscissor/game/SettingsTest.java
@@ -8,18 +8,18 @@ public class SettingsTest {
 
     private Player testPlayer = new Player("1", () -> Gesture.ROCK);
 
-    @Test(expected = AssertionError.class)
+    @Test(expected = IllegalArgumentException.class)
     public void zeroRoundsToPlayRaisesAnException() throws Exception {
         new Settings(0, null, null);
     }
 
 
-    @Test(expected = AssertionError.class)
+    @Test(expected = IllegalArgumentException.class)
     public void missingFirstPlayerRaisesAnException() {
         new Settings(1, null, null);
     }
 
-    @Test(expected = AssertionError.class)
+    @Test(expected = IllegalArgumentException.class)
     public void missingSecondPlayerRaisesAnException() throws Exception {
         new Settings(1, testPlayer, null);
     }
